c2f35434d05eebd5e1f0ab64c028dd88f70846e6
Camila Fonseca <camilafonseca.ang7@gmail.com>
2021-01-07 16:32:36 -0300

    Added animations

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 $(function(){
+    function AnimateRotate(element) {
+        const angle = 0
+        const animationSize = window.innerWidth * 60 / 100
+        const imagePerimeter = 120 * Math.PI
+        const numberOfLaps = animationSize / imagePerimeter
+        const lapDegrees = numberOfLaps * 360
+        const startsFrom = lapDegrees * -1
+
+        $({deg: startsFrom}).animate({deg: angle}, {
+            duration: 2000,
+            step: function(now) {
+                element.css({
+                    transform: 'rotate(' + now + 'deg)'
+                })
+            }
+        })
+    }
+    
+
     const inputEl = $('input')
     const buttonEl = $('button')
     const containerEl = $('#container')
     const apiUrl = 'https://api.github.com/users/'
-
+    
     const callback = () => {
         containerEl.html('<p>Loading...</p>')
         $.getJSON(apiUrl + inputEl.val(), data => {
             containerEl.html(`
-                <h1>${data.name}</h1>
+                <h1>${data.name || 'User name not found!'}</h1>
                 <img src="${data.avatar_url}" alt="User Image" />
-                <p>${data.bio}</p>
+                <p class="in-animation">${data.bio || 'User bio not found!'}</p>
             `)
-        }).catch(() => containerEl.html('User not found!'))
+            
+            const imageEl = $('img')
+
+            $('h1').fadeIn('slow')
+            $('p').fadeIn('slow')
+
+            imageEl.animate({left: '0'}, 2000)
+            AnimateRotate(imageEl)
+        }).catch(() => containerEl.html('User not found! :('))
     }
     
     buttonEl.click(callback)
